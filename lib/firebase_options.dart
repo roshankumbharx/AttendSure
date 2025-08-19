0510e962be89376b471a072f0b4d9398b2bd154b
roshankumbharx <150536457+roshankumbharx@users.noreply.github.com>
2025-08-20 00:25:14 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -39,19 +39,19 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyA7GZ8QImg0FUym9cvnW8qVjBAjYM3nhHw',
-    appId: '1:250970777186:android:54026e554bf6c0f38b533b',
-    messagingSenderId: '250970777186',
-    projectId: 'fir-testing-ac295',
-    storageBucket: 'fir-testing-ac295.appspot.com',
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    storageBucket: '',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyB-Ol8nQns7qQfq90qkQ54NB6LmlwG84q0',
-    appId: '1:250970777186:ios:554dc89cd4c585678b533b',
-    messagingSenderId: '250970777186',
-    projectId: 'fir-testing-ac295',
-    storageBucket: 'fir-testing-ac295.appspot.com',
-    iosBundleId: 'com.example.attendsure',
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    storageBucket: '',
+    iosBundleId: '',
   );
 }
